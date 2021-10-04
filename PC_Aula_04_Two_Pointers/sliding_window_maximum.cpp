80003f0d9d3e485e59590d216cd9036dd0a282d8
MisaelVM <mvilchezmo@unsa.edu.pe>
2021-10-04 18:32:11 -0500

    Programa mejorado

--- PC_Aula_04_Two_Pointers/sliding_window_maximum.cpp
+++ PC_Aula_04_Two_Pointers/sliding_window_maximum.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <deque>
 #include <vector>
-
-std::vector<int> prefix_sum(std::vector<int>& v) {
-	std::vector<int> prefix;
-	prefix.reserve((int)v.size() + 1);
-	prefix.push_back(0);
-	for (auto& i : v)
-		prefix.push_back(prefix.back() + i);
-	return prefix;
-}
 
 std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k) {
 	std::vector<int> max; max.reserve((int)nums.size() - k + 1);
-	int max_value;
-	for (int i = 0; i < (int)nums.size() - k + 1; ++i) {
-		max_value = nums[i];
-		for (int j = i + 1; j < i + k; ++j)
-			if (nums[j] > max_value)
-				max_value = nums[j];
-		max.push_back(max_value);
+	std::deque<int> dque;
+	for (int i = 0; i < k; ++i) {
+		while (!dque.empty() && nums[i] >= nums[dque.back()])
+			dque.pop_back();
+		dque.push_back(i);
 	}
+	max.push_back(nums[dque.front()]);
+
+	for (int i = k; i < (int)nums.size(); ++i) {
+		while (!dque.empty() && i - k >= dque.front())
+			dque.pop_front();
+		while (!dque.empty() && nums[i] >= nums[dque.back()])
+			dque.pop_back();
+		dque.push_back(i);
+		max.push_back(nums[dque.front()]);
+	}
+
 	return max;
 }
 
@@ -34,8 +35,40 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 
-	std::vector<int> nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
-	int k = 3;
+	std::vector<int> nums;
+	int k;
+	// Caso de prueba 1
+	// Entrada:
+	// 1 3 -1 -3 5 3 6 7
+	// 3
+	// Salida:
+	// 3 3 5 6 7
+	//
+	nums = { 1, 3, -1, -3, 5, 3, 6, 7 };
+	k = 3;
 	print_vector(maxSlidingWindow(nums, k));
+
+	// Caso de prueba 2
+	// Entrada:
+	// 9 11
+	// 2
+	// Salida:
+	// 11
+	//
+	nums = { 9, 11 };
+	k = 2;
+	print_vector(maxSlidingWindow(nums, k));
+
+	// Caso de prueba 3
+	// Entrada:
+	// 1 -1
+	// 1
+	// Salida:
+	// 1 -1
+	//
+	nums = { 1, -1 };
+	k = 1;
+	print_vector(maxSlidingWindow(nums, k));
+
 	return 0;
 }
