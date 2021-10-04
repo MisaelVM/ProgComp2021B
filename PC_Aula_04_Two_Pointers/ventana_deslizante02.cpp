3ac59f5dbf99bc30cc46249086c4753201cdbff4
MisaelVM <mvilchezmo@unsa.edu.pe>
2021-10-04 16:50:10 -0500

    Programa ventana deslizante 2

--- PC_Aula_04_Two_Pointers/ventana_deslizante02.cpp
+++ PC_Aula_04_Two_Pointers/ventana_deslizante02.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 
 int smallest_contiguous_subarray_geq(std::vector<int>& nums, int S) {
-	int i = -1, j, min_size = (int)nums.size() + 1, cum_sum = 0;
-	for (j = 0; j < (int)nums.size(); ++j) {
+	int i = 0, j = 0, min_size = (int)nums.size() + 1, cum_sum = 0;
+	while (j < (int)nums.size()) {
 		cum_sum += nums[j];
 		if (cum_sum >= S) {
-			cum_sum = 0;
-			i = j;
-			if (j - i < min_size)
-				min_size = j - i;
+			if (j - i + 1 < min_size)
+				min_size = j - i + 1;
+			cum_sum -= (nums[i] + nums[j]);
+			++i;
 		}
+		else
+			++j;
 	}
 	return min_size;
 }
@@ -20,8 +22,15 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 
-	std::vector<int> nums = { 1, 2, 3, 4, 5 };
-	int S = 5;
+	// Caso de prueba 1
+	// Entrada:
+	// 2 4 2 5 1
+	// 7
+	// Salida:
+	// 2
+	//
+	std::vector<int> nums = { 2, 4, 2, 5, 1 };
+	int S = 7;
 	std::cout << smallest_contiguous_subarray_geq(nums, S) << "\n";
 
 	return 0;
