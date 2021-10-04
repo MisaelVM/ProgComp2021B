3c477322108049ed26063202ebc1adb5d27c905f
MisaelVM <mvilchezmo@unsa.edu.pe>
2021-10-04 14:47:25 -0500

    Corrigiendo programa

--- PC_Aula_04_Two_Pointers/sum_two_values.cpp
+++ PC_Aula_04_Two_Pointers/sum_two_values.cpp
@@ -1,38 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <utility>
-
-std::pair<int, int> find_numbers(std::vector<int>& nums, int k) {
-	int i = 0, j = nums.size() - 1;
-	while (j > i) {
-		int sum = nums[i] + nums[j];
-		if (sum == k)
-			return std::pair<int, int>(i, j);
-		else if (sum > k)
-			--j;
-		else
-			++i;
+ 
+std::pair<int, int> two_sum(std::vector<int>& nums, int k) {
+	std::unordered_map<int, int> H;
+	for (int i = 0; i < (int)nums.size(); ++i) {
+		if (H.find(k - nums[i]) != H.end())
+			return std::pair<int, int>(H[k - nums[i]], i);
+		H[nums[i]] = i;
 	}
 	return std::pair<int, int>(-1, 0);
 }
-
+ 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(nullptr);
-
-    std::vector<int> nums;
+ 
+	std::vector<int> nums;
 	int n, x, v;
-    std::cin >> n >> x;
-    nums.reserve(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> v;
-        nums.push_back(v);
-    }
-
-    std::pair<int, int> pos = find_numbers(nums, x);
-    (pos.first != -1) ? std::cout << pos.first + 1 << " " << pos.second + 1 : std::cout << "IMPOSSIBLE";
-    std::cout << "\n";
-
+	std::cin >> n >> x;
+	nums.reserve(n);
+	for (int i = 0; i < n; ++i) {
+		std::cin >> v;
+		nums.push_back(v);
+	}
+ 
+	std::pair<int, int> pos = two_sum(nums, x);
+	(pos.first != -1) ? std::cout << pos.first + 1 << " " << pos.second + 1 : std::cout << "IMPOSSIBLE";
+	std::cout << "\n";
+ 
 	return 0;
 }
