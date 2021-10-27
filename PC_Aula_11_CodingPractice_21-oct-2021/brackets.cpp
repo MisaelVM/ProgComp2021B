9e42ac644384a2903b210cc2aac155a58355f9de
MisaelVM <mvilchezmo@unsa.edu.pe>
2021-10-27 01:41:58 -0500

    Problema Brackets resuelto mediante pilas

---
+++ PC_Aula_11_CodingPractice_21-oct-2021/brackets.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int solution(std::string& S) {
+	std::stack<char> validator;
+	for (auto& i : S) {
+		switch (i) {
+		case '(':
+		case '[':
+		case '{':
+			validator.push(i);
+			break;
+		case ')':
+			if (validator.empty() || validator.top() != '(')
+				return 0;
+			validator.pop();
+			break;
+		case ']':
+			if (validator.empty() || validator.top() != '[')
+				return 0;
+			validator.pop();
+			break;
+		case '}':
+			if (validator.empty() || validator.top() != '{')
+				return 0;
+			validator.pop();
+			break;
+		default:
+			return 0;
+		}
+	}
+	return validator.empty();
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	std::string S;
+
+	// Caso de prueba:
+	// Entrada:
+	// {[()()]}
+	// Salida:
+	// 1
+	//
+	S = "{[()()]}";
+	std::cout << solution(S) << "\n";
+
+	// Caso de prueba:
+	// Entrada:
+	// ([)()]
+	// Salida:
+	// 0
+	//
+	S = "([)()]";
+	std::cout << solution(S) << "\n";
+
+	return 0;
+}
